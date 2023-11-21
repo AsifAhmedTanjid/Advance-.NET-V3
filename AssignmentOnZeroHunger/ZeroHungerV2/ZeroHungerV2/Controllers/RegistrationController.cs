598783cae86f0e98beabbaffe2e439460aab5088
AsifAhmedTanjid <asifahmednirob44@gmail.com>
2023-11-22 05:40:47 +0600

    zerohunger

---
+++ AssignmentOnZeroHunger/ZeroHungerV2/ZeroHungerV2/Controllers/RegistrationController.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ZeroHungerV2.EF;
+
+namespace ZeroHungerV2.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        // GET: Registration
+        [HttpGet]
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult SignUp(User u)
+        {
+            var db = new ZeroHungerDBV2Entities();
+            db.Users.Add(u);
+            db.SaveChanges();
+            return RedirectToAction("Login");
+
+        }
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(User u)
+        {
+            var db= new ZeroHungerDBV2Entities();
+            var data = (from us in db.Users
+                        where us.Username == u.Username && us.Password == u.Password
+                        select us).SingleOrDefault();
+            if (data != null)
+            {
+                Session["id"]=data.ID; 
+                Session["name"]=data.Name; 
+                Session["username"]=data.Username; 
+                Session["password"]=data.Password;
+                Session["phone"]=data.Phone;
+                Session["email"]=data.Email;
+                Session["address"]=data.Address;
+                Session["role"]=data.Role;
+                Session["status"]=data.Status;
+                
+                if(data.Role=="employee")
+                {
+
+                    return RedirectToAction("Dashboard", "Employee");
+
+                }
+
+                else if (data.Role == "admin")
+                {
+
+                    return RedirectToAction("Dashboard", "Admin");
+
+                }
+
+                else if (data.Role == "restaurant")
+                {
+                    var resdata = (from r in db.Restaurants
+                                where r.RestaurantOwnerID == data.ID 
+                                select r).SingleOrDefault();
+                    if (resdata != null)
+                    {
+                        Session["restaurantid"] = resdata.RestaurantID;
+                        Session["restaurantname"] = resdata.RestaurantName;
+                        Session["restaurantlocation"] = resdata.RestaurantLocation;
+                        Session["restaurantcontactperson"] = resdata.RestaurantContactPerson;
+                        Session["restaurantcontactnumber"] = resdata.RestaurantContactNumber;
+                        Session["restaurantemail"] = resdata.RestaurantEmail;
+                        Session["restaurantownerid"] = resdata.RestaurantOwnerID;
+
+
+                        return RedirectToAction("Dashboard", "Restaurant");
+                    }
+
+                    
+
+                }
+
+            }
+
+            return RedirectToAction("SignUp","Registration");
+        }
+
+    }
+}
---
+++ AssignmentOnZeroHunger/ZeroHungerV2/ZeroHungerV2/Controllers/RestaurantController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ZeroHungerV2.Auth;
+using ZeroHungerV2.EF;
+
+namespace ZeroHungerV2.Controllers
+{
+    public class RestaurantController : Controller
+    {
+        // GET: Restaurant
+        [Logged]
+        public ActionResult Dashboard()
+        {
+            return View();
+        }
+        [Logged]
+        [HttpGet]
+        public ActionResult CreateNewRequest()
+        {
+            return View();
+        }
+        [HttpPost]
+
+        public ActionResult CreateNewRequest(Request request)
+        {
+            var db = new ZeroHungerDBV2Entities();
+            db.Requests.Add(request);
+            db.SaveChanges();
+            return RedirectToAction("PendingRequest");
+        }
+        [Logged]
+        public ActionResult PendingRequest()
+        {
+            var id = Convert.ToInt32(Session["restaurantid"]);
+            var db = new ZeroHungerDBV2Entities();
+            var data = (from requests in db.Requests
+                        where requests.RestaurantID == id && requests.Status == "Requested"
+                        select requests).ToList();
+
+            return View(data);
+        }
+        public ActionResult DeleteRequest(int id) 
+        {
+            var db = new ZeroHungerDBV2Entities();
+            var data = db.Requests.Find(id);
+            db.Requests.Remove(data);
+            db.SaveChanges();
+
+            return RedirectToAction("PendingRequest");
+        }
+        [Logged]
+        public ActionResult DonateHistory()
+        {
+            var id = Convert.ToInt32(Session["restaurantid"]);
+            var db = new ZeroHungerDBV2Entities();
+            var data = (from requests in db.Requests
+                        where requests.RestaurantID == id && requests.Status == "Completed"
+                        select requests).ToList();
+
+            return View(data);
+        }
+    }
+}
