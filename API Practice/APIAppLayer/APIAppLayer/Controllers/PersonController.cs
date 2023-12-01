17e31c54494dbc278f6b99c870541bf18bd86a4b
AsifAhmedTanjid <asifahmednirob44@gmail.com>
2023-12-02 00:36:58 +0600

    3layeradded

---
+++ API Practice/APIAppLayer/APIAppLayer/Controllers/PersonController.cs
@@ -0,0 +1,31 @@
+﻿using BLL.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace APIAppLayer.Controllers
+{
+    public class PersonController : ApiController
+    {
+
+        [HttpGet]
+        [Route("api/name/{id}")]
+        public HttpResponseMessage GetName (int id)
+        {
+
+            try
+            {
+                var data =PersonService.GetName (id);
+                return Request.CreateResponse (HttpStatusCode.OK, data);
+
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex.Message);
+            }
+        }
+    }
+}
